4b7e9a47b5a2f0f2c35a790a16975c7b5a241690
PoojaThatireddy <pooja.thatireddy@appitventures.com>
2017-08-09 19:51:43 +0530

    added url media player

--- app/src/main/java/com/appit/AnimationsAndBottomSheet/MusicPlayer.java
+++ app/src/main/java/com/appit/AnimationsAndBottomSheet/MusicPlayer.java
@@ -1,10 +1,17 @@
 package com.appit.AnimationsAndBottomSheet;
 
+import android.app.ProgressDialog;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
+
+import java.io.IOException;
 
 public class MusicPlayer extends AppCompatActivity {
     private Button btn;
@@ -22,9 +29,128 @@ public class MusicPlayer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player);
 
-        btn = (Button)findViewById(R.id.button1);
+
+        btn=(Button)findViewById(R.id.button1);
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         btn.setOnClickListener(pausePlay);
+    }
+
+
+    private View.OnClickListener pausePlay = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            // TODO Auto-generated method stub
+
+            if (!playPause) {
+                btn.setBackgroundResource(R.drawable.button_pause);
+                if (intialStage)
+                    new Player()
+                            .execute("http://www.virginmegastore.me/Library/Music/CD_001214/Tracks/Track1.mp3");
+                else {
+                    if (!mediaPlayer.isPlaying())
+                        mediaPlayer.start();
+                }
+                playPause = true;
+            } else {
+                btn.setBackgroundResource(R.drawable.button_play);
+                if (mediaPlayer.isPlaying())
+                    mediaPlayer.pause();
+                playPause = false;
+            }
+        }
+    };
+    /**
+     * preparing mediaplayer will take sometime to buffer the content so prepare it inside the background thread and starting it on UI thread.
+     * @author piyush
+     *
+     */
+
+    class Player extends AsyncTask<String, Void, Boolean> {
+        private ProgressDialog progress;
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            Boolean prepared;
+            try {
+
+                mediaPlayer.setDataSource(params[0]);
+
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        // TODO Auto-generated method stub
+                        intialStage = true;
+                        playPause=false;
+                        btn.setBackgroundResource(R.drawable.button_play);
+                        mediaPlayer.stop();
+                        mediaPlayer.reset();
+                    }
+                });
+                mediaPlayer.prepare();
+                prepared = true;
+            } catch (IllegalArgumentException e) {
+                // TODO Auto-generated catch block
+                Log.d("IllegarArgument", e.getMessage());
+                prepared = false;
+                e.printStackTrace();
+            } catch (SecurityException e) {
+                // TODO Auto-generated catch block
+                prepared = false;
+                e.printStackTrace();
+            } catch (IllegalStateException e) {
+                // TODO Auto-generated catch block
+                prepared = false;
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                prepared = false;
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return prepared;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(result);
+            if (progress.isShowing()) {
+                progress.cancel();
+            }
+            Log.d("Prepared", "//" + result);
+            mediaPlayer.start();
+
+            intialStage = false;
+        }
+
+        public Player() {
+            progress = new ProgressDialog(MainActivity.this);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+            this.progress.setMessage("Buffering...");
+            this.progress.show();
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        if (mediaPlayer != null) {
+            mediaPlayer.reset();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
